f48e4f2f0aa08c661a478bc92622ad21218a6c11
Leo <leorojas22@gmail.com>
2021-01-15 15:49:04 -0500

    fix

--- DanakiDoctrineEnumTypeBundle.php
+++ DanakiDoctrineEnumTypeBundle.php
@@ -9,18 +9,6 @@ class DanakiDoctrineEnumTypeBundle extends Bundle
 {
     public function boot()
     {
-        // Merge types and custom types
-        if (!empty($config['custom_types'])) {
-            foreach ($config['custom_types'] as $customPhpEnumTypeClass => $customTypes) {
-                foreach ($customTypes as $enumTypeName => $enumClass) {
-                    $typeName = is_string($enumTypeName) ? $enumTypeName : $enumClass;
-                    $config['types'][$typeName] = [
-                        'enum_class' => $enumClass,
-                        'type_class' => $customPhpEnumTypeClass
-                    ];
-                }
-            }
-        }
 
         foreach ($this->container->getParameter('danaki_doctrine_enum_type.types') as $typeName => $enumConfig) {
             $typeName = \is_string($typeName) ? $typeName : $enumConfig['enum_class'];
--- DependencyInjection/DanakiDoctrineEnumTypeExtension.php
+++ DependencyInjection/DanakiDoctrineEnumTypeExtension.php
@@ -9,6 +9,19 @@ class DanakiDoctrineEnumTypeExtension extends ConfigurableExtension
 {
     protected function loadInternal(array $config, ContainerBuilder $container)
     {
+        // Merge types and custom types
+        if (!empty($config['custom_types'])) {
+            foreach ($config['custom_types'] as $customPhpEnumTypeClass => $customTypes) {
+                foreach ($customTypes as $enumTypeName => $enumClass) {
+                    $typeName = is_string($enumTypeName) ? $enumTypeName : $enumClass;
+                    $config['types'][$typeName] = [
+                        'enum_class' => $enumClass,
+                        'type_class' => $customPhpEnumTypeClass
+                    ];
+                }
+            }
+        }
+
         $container->setParameter('danaki_doctrine_enum_type.types', $config['types']);
     }
 }
